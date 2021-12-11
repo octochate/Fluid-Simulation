43df3bfdaa587332b1ec174d1eacc893e1f2135a
Runtime-Learner <matthias15002@gmail.com>
2021-12-11 03:49:15 -0500

    Add testing parameters. Add comments

--- fluidSim_cuda/fluidSim_cuda/kernel.cu
+++ fluidSim_cuda/fluidSim_cuda/kernel.cu
@@ -68,7 +68,6 @@ __global__ void lin_solve(int N, int b, float* x, float* x0, float a, float c, i
     }
 }
 
-
 __global__ void advect(int N, int b, float* d, float* d0, float* u, float* v, float dt, int elementPerThread)
 {
     int idx = (blockIdx.x * blockDim.x + threadIdx.x) * elementPerThread;
@@ -100,7 +99,6 @@ __global__ void advect(int N, int b, float* d, float* d0, float* u, float* v, fl
     __syncthreads();
     set_bnd(N, b, d, idx, elementPerThread);
 }
-
 
 __global__ void project1(int N, float* u, float* v, float* p, float* div, int elementPerThread)
 {
@@ -194,19 +192,61 @@ static void printFrameMatrices(float* dens, float* u, float* v, int N)
 
 int main(int argc, char* argv[]) {
 
-    int N = 5;
+    // declare variables
     float dt = 0.1f, diff = 0.1f, visc = 0.1f;
     float force = 100.0f, source = 100.0f;
+    int N = 5, num_threads = 2, num_iterations = 10, display_output = 0, use_streams = 0;
 
-    // Allocate space for host
+    // load parameters from command line
+    if (argc != 6) {
+        fprintf(stderr, "usage : %s <grid size> <number of threads> <number of iterations> <display output> <use CUDA streams>\n", argv[0]);
+        fprintf(stderr, "where:\n"); \
+        fprintf(stderr, "\t grid_size                : grid resolution\n");
+        fprintf(stderr, "\t number_of_threads        : number of threads running per kernel (1 block/kernel enforced)\n");
+        fprintf(stderr, "\t number_of_iterations     : number of iterations to run the simulation\n");
+        fprintf(stderr, "\t display_output           : Whether to display the opdated grids after every iteration\n");
+        fprintf(stderr, "\t use_CUDA_streams         : Whether to use CUDA streams or have all kernels run sequentially\n");
+        exit(1);
+    }
+
+
+    N = atoi(argv[1]);
+    num_threads = atoi(argv[2]);
+    num_iterations = atoi(argv[3]);
+    display_output = atoi(argv[4]);
+    use_streams = atoi(argv[5]);
+
+    // validate parameters
+    if (N <= 0) {
+        fprintf(stderr, "grid_size must be an integer larger than 0\n");
+        exit(1);
+    }
+    if (num_threads <= 0 || num_threads > 1024) {
+        fprintf(stderr, "number_of_threads must be an integer in the range [1, 1024]\n");
+        exit(1);
+    }
+    if (num_iterations <= 0) {
+        fprintf(stderr, "number_of_iterations must be an integer larger than 0\n");
+        exit(1);
+    }
+    if (display_output != 0 && display_output != 1) {
+        fprintf(stderr, "display_output must be 1 if TRUE and 0 if FALSE\n");
+        exit(1);
+    }
+    if (use_streams != 0 && use_streams != 1) {
+        fprintf(stderr, "use_streams must be 1 if TRUE and 0 if FALSE\n");
+        exit(1);
+    }
+
+    
     int size = (N + 2) * (N + 2);
 
-    // Device copies of data
+    // unified memory: pointers to data
     float* u_cuda, * v_cuda, * u_prev_cuda, * v_prev_cuda;
     float* p_cuda, * div_cuda;
     float* dens_cuda, * dens_prev_cuda;
 
-    // Allocate space for device copies
+    // Allocate space for arrays
     cudaMallocManaged(&u_cuda, sizeof(float) * size);
     cudaMallocManaged(&v_cuda, sizeof(float) * size);
     cudaMallocManaged(&p_cuda, sizeof(float) * size);
@@ -216,6 +256,7 @@ int main(int argc, char* argv[]) {
     cudaMallocManaged(&dens_cuda, sizeof(float) * size);
     cudaMallocManaged(&dens_prev_cuda, sizeof(float) * size);
 
+    // Initialize all arrays to 0
     cudaMemset(u_cuda, 0, sizeof(float) * size);
     cudaMemset(v_cuda, 0, sizeof(float) * size);
     cudaMemset(p_cuda, 0, sizeof(float) * size);
@@ -225,110 +266,99 @@ int main(int argc, char* argv[]) {
     cudaMemset(dens_cuda, 0, sizeof(float) * size);
     cudaMemset(dens_prev_cuda, 0, sizeof(float) * size);
 
-    // have copy of host data
-    get_from_UI(dens_prev_cuda, u_prev_cuda, v_prev_cuda, force, source, N);
 
-
-    // Parallelize computation :: TODO update values to match
+    // Parallelization scheme for add_source
     int num_threads_source = (N + 2);
     int num_blocks_source = (N + 2);
-    int num_threads = 2;
+    
+    // Parallelization scheme for only using one block
     int elementsPerThread = (N * N + 1) / num_threads;
 
+    // Instantiate cuda streams
     cudaStream_t stream1, stream2, stream3;
     cudaStreamCreate(&stream1);
-    cudaStreamCreate(&stream2);
-    cudaStreamCreate(&stream3);
+    if (use_streams) {
+        cudaStreamCreate(&stream2);
+        cudaStreamCreate(&stream3);
+    }
+    else {
+        stream2 = stream1;
+        stream3 = stream1;
+    }
+    
 
-    puts("before add source");
-    printFrameMatrices(dens_cuda, u_cuda, v_cuda, N);
+
+    // update loop
+    for (int i = 0; i < num_iterations; i++) {
+
+        // get input from 'GUI'
+        get_from_UI(dens_prev_cuda, u_prev_cuda, v_prev_cuda, force, source, N);
 
 
-    int numIterations = 10;
-    for (int i = 0; i < numIterations; i++) {
-
-        //// Velocity timestep parallelization
+        //// Add GUI inputs to grids
         add_source <<< num_blocks_source, num_threads_source >>> (u_cuda, u_prev_cuda, dt, size);
         add_source <<< num_blocks_source, num_threads_source >>> (v_cuda, v_prev_cuda, dt, size);
         add_source <<< num_blocks_source, num_threads_source >>> (dens_cuda, dens_prev_cuda, dt, size);
         cudaDeviceSynchronize();
 
-        /*puts("after add source");
-        printFrameMatrices(dens_cuda, u_cuda, v_cuda, N);*/
-
         SWAP(u_prev_cuda, u_cuda);
         SWAP(v_prev_cuda, v_cuda);
         SWAP(dens_prev_cuda, dens_cuda);
 
-        //// diffuse
+        //// diffuse step
         float a = dt * visc * N * N;
         lin_solve << < 1, num_threads, 0, stream1 >> > (N, 1, u_cuda, u_prev_cuda, a, 1 + 4 * a, elementsPerThread); // diffuse u_cuda
         lin_solve << < 1, num_threads, 0, stream2 >> > (N, 2, v_cuda, v_prev_cuda, a, 1 + 4 * a, elementsPerThread); // diffuse v_cuda
-
         a = dt * diff * N * N;
         lin_solve << < 1, num_threads, 0, stream3 >> > (N, 0, dens_cuda, dens_prev_cuda, a, 1 + 4 * a, elementsPerThread); // diffuse dens_cuda
         cudaDeviceSynchronize();
 
 
-   /*     puts("after diffuse");
-        printFrameMatrices(dens_cuda, u_cuda, v_cuda, N);*/
-
         // projection step (no swapping beforehand)
         project1 << < 1, num_threads >> > (N, u_cuda, v_cuda, p_cuda, div_cuda, elementsPerThread);
-        cudaDeviceSynchronize();
         lin_solve << < 1, num_threads >> > (N, 0, p_cuda, div_cuda, 1, 4, elementsPerThread);
-        cudaDeviceSynchronize();
         project3 << < 1, num_threads >> > (N, u_cuda, v_cuda, p_cuda, elementsPerThread);
         cudaDeviceSynchronize();
 
-     /*   puts("after project 1");
-        printFrameMatrices(dens_cuda, u_cuda, v_cuda, N);*/
-
         SWAP(u_prev_cuda, u_cuda);
         SWAP(v_prev_cuda, v_cuda);
-    
 
+        // advection step (velocity grid)
         advect << < 1, num_threads, 0, stream1 >> > (N, 1, u_cuda, u_prev_cuda, u_prev_cuda, v_prev_cuda, dt, elementsPerThread);
         advect << < 1, num_threads, 0, stream2 >> > (N, 2, v_cuda, v_prev_cuda, u_prev_cuda, v_prev_cuda, dt, elementsPerThread);
         cudaDeviceSynchronize();
 
-      /*  puts("after advect (only velocities)");
-        printFrameMatrices(dens_cuda, u_cuda, v_cuda, N);*/
-
 
         // projection step (no swapping beforehand)
         project1 << < 1, num_threads >> > (N, u_cuda, v_cuda, p_cuda, div_cuda, elementsPerThread);
-        cudaDeviceSynchronize();
         lin_solve << < 1, num_threads >> > (N, 0, p_cuda, div_cuda, 1, 4, elementsPerThread);
-        cudaDeviceSynchronize();
         project3 << < 1, num_threads >> > (N, u_cuda, v_cuda, p_cuda, elementsPerThread);
         cudaDeviceSynchronize();
-
-        //puts("after project 2");
-        //printFrameMatrices(dens_cuda, u_cuda, v_cuda, N);
-
-
     
 
-        // Density timestep parallelization
+        // advection step (density grid)
         SWAP(dens_prev_cuda, dens_cuda);
         advect << < 1, num_threads >> > (N, 0, dens_cuda, dens_prev_cuda, u_cuda, v_cuda, dt, elementsPerThread);
         cudaDeviceSynchronize();
 
-        // Copy result back to host
-        puts("after advect (density)");
-        printFrameMatrices(dens_cuda, u_cuda, v_cuda, N);
-
-        puts("\n");
+        // Display results if needed
+        printf("Finished iteration %d:\n", i);
+        if (display_output) {
+            printFrameMatrices(dens_cuda, u_cuda, v_cuda, N);
+            puts("\n");
+        }
     }
 
     // Free Device space
     cudaFree(u_cuda);
     cudaFree(v_cuda);
     cudaFree(u_prev_cuda);
+    cudaFree(v_prev_cuda);
     cudaFree(dens_cuda);
     cudaFree(dens_prev_cuda);
-    cudaFree(v_prev_cuda);
+    cudaFree(p_cuda);
+    cudaFree(div_cuda);
+    
 
     // Destroy cuda streams
     cudaStreamDestroy(stream1);
